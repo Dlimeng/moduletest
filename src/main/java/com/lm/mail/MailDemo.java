3efea79924184369f3f64573109e78409410077f
77954309 <77954309@qq.com>
2021-01-21 18:35:32 +0800

    mail

---
+++ src/main/java/com/lm/mail/MailDemo.java
@@ -0,0 +1,93 @@
+package com.lm.mail;
+
+import com.sun.mail.smtp.SMTPTransport;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import java.util.Properties;
+import javax.mail.Transport;
+/**
+ * @Classname MailDemo
+ * @Description TODO
+ * @Date 2021/1/21 15:59
+ * @Created by limeng
+ */
+public class MailDemo {
+    public static String myEmailAccount = "zabbix@knowlegene.com";
+    public static String myEmailPassword = "know@2020";
+    public static String myEmailSMTPHost = "smtp.knowlegene.com";
+
+    public static String receiveMailAccount = "77954309@qq.com";
+
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();                    // 参数配置
+        props.setProperty("mail.transport.protocol", "smtp");   // 使用的协议（JavaMail规范要求）
+        props.setProperty("mail.smtp.host", myEmailSMTPHost);   // 发件人的邮箱的 SMTP 服务器地址
+        props.setProperty("mail.smtp.auth", "true");            // 需要请求认证
+
+
+
+        props.put("mail.smtp.port", 25);
+        props.put("mail.smtp.timeout", 1000);
+        props.put("mail.smtp.connectiontimeout", 1000);
+        props.put("mail.smtp.starttls.enable", true);
+        //props.put("mail.smtp.ssl.trust", "*");
+
+
+        Session session = Session.getInstance(props);
+        session.setDebug(true);                                 // 设置为debug模式, 可以查看详细的发送 log
+
+
+        // 3. 创建一封邮件
+        MimeMessage message = createMimeMessage(session, myEmailAccount, receiveMailAccount);
+
+        // 4. 根据 Session 获取邮件传输对象
+        SMTPTransport transport = (SMTPTransport)session.getTransport("smtp");
+
+        transport.connect(myEmailSMTPHost, 25,myEmailAccount,myEmailPassword);
+
+        // 6. 发送邮件, 发到所有的收件地址, message.getAllRecipients() 获取到的是在创建邮件对象时添加的所有收件人, 抄送人, 密送人
+        transport.sendMessage(message, message.getAllRecipients());
+
+        // 7. 关闭连接
+        transport.close();
+
+    }
+
+    /**
+     * 创建一封只包含文本的简单邮件
+     *
+     * @param session 和服务器交互的会话
+     * @param sendMail 发件人邮箱
+     * @param receiveMail 收件人邮箱
+     * @return
+     * @throws Exception
+     */
+    public static MimeMessage createMimeMessage(Session session, String sendMail, String receiveMail) throws Exception {
+        // 1. 创建一封邮件
+        MimeMessage message = new MimeMessage(session);
+
+        // 2. From: 发件人（昵称有广告嫌疑，避免被邮件服务器误认为是滥发广告以至返回失败，请修改昵称）
+        message.setFrom(new InternetAddress(sendMail, "某宝网", "UTF-8"));
+
+        // 3. To: 收件人（可以增加多个收件人、抄送、密送）
+        message.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(receiveMail, "XX用户", "UTF-8"));
+
+        // 4. Subject: 邮件主题（标题有广告嫌疑，避免被邮件服务器误认为是滥发广告以至返回失败，请修改标题）
+        message.setSubject("打折钜惠", "UTF-8");
+
+        // 5. Content: 邮件正文（可以使用html标签）（内容有广告嫌疑，避免被邮件服务器误认为是滥发广告以至返回失败，请修改发送内容）
+        message.setContent("XX用户你好, 今天全场5折, 快来抢购, 错过今天再等一年。。。", "text/html;charset=UTF-8");
+
+        // 6. 设置发件时间
+        message.setSentDate(new Date());
+
+        // 7. 保存设置
+        message.saveChanges();
+
+        return message;
+    }
+}
